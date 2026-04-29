f04bd65c34bdbc324c69eadeadcf888f303bf32a
yokko <yokko@nixos>
2026-04-29 10:11:53 +0800

    auto: Ср 29 апр 2026 10:11:53 +08

--- home-modules/fish.nix
+++ home-modules/fish.nix
@@ -4,6 +4,12 @@
     enable = true;
 
     interactiveShellInit = builtins.readFile ../dotfiles/fish/config.fish;
+
+    functions = {
+      fish_greeting = "function fish_greeting
+    fastfetch --key-padding-left 5
+end";
+    };
   };
 
   xdg.configFile."fish/conf.d/nvm.fish".source = ../dotfiles/fish/conf.d/nvm.fish;
